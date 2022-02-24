21170707afe4838e6974c377eb8469888e492437
nicoslaptopW <nvog1@gcloud.ua.es>
2022-02-24 20:32:09 +0100

    restored files

--- Contributor.java
+++ Contributor.java
@@ -1,24 +1,14 @@
 import java.util.*;
 
-public class Contributor {
-    private String name;
+public class contributor {
     private ArrayList<String> skills;
-    private ArrayList<Integer> levels;
+    private ArrayList<Integer> level;
     private static ArrayList<contributor> contributors = new ArrayList<>();
 
-    public Contributor(String n, ArrayList<String> s, ArrayList<Integer> l) {
-        name = n;
+    public contributor(ArrayList<String> s, ArrayList<Integer> l) {
         skills = s;
-        levels = l;
+        level = l;
         contributors.add(this);
     }
 
-    public static mostrarTodos() {
-        for(int i=0; i<contributors.size(); i++) {
-            System.out.println("Contributor: " + contributors.get(i).name);
-            for(int j=0; j<contributors.get(i).skills.size(); j++) {
-                System.out.println("\t" + contributors.get(i).skills.get(j) + " > " + contributors.get(i).levels.get(j))
-            }
-        }
-    }
 }
